6037f9788491ba73f48bceec8761f43d43e44417
Anurag-Bharati <anuragbharati26@gmail.com>
2023-06-07 15:02:59 +0545

    SSE test: For updating the model

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
+import asyncio
+import random
 
-from fastapi import FastAPI, Response, status
+from fastapi import FastAPI, Response, Request, status
 from pydantic import BaseModel
 from app.model.model import predict_orders
 from app.model.model import __version__ as model_version
 from typing import List
+from fastapi.responses import StreamingResponse
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -41,3 +44,19 @@ def predict(payload: DataIn, response: Response):
     return {"success": True, "predicted_orders": orders.tolist()}
 
 
+@app.get("/sse")
+async def sse_endpoint(request: Request):
+    global core_updating
+    # TODO update core (ML Model) w/ new data
+    if core_updating:
+        return {"success": False, "message": "Core is updating. Please wait"}
+    core_updating = True
+
+    async def event_generator():
+        global core_updating
+        # logic to generate SSE events goes here
+        for i in range(100):
+            await asyncio.sleep(random.random()/2)
+            yield f"Updating core: {i}% \n"
+        core_updating = False
+    return StreamingResponse(event_generator(), media_type="text/event-stream", headers={"Cache-Control": "no-cache"})
