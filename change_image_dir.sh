523edc61248a9b09f933e2bd4e23c563314c8e7e
Anurag-Bharati <anuragbharati26@gmail.com>
2023-06-08 22:03:53 +0545

    Configured docker for future deployment

---
+++ change_image_dir.sh
@@ -0,0 +1,4 @@
+#wsl --shutdown
+#wsl --export docker-desktop-data D:/docker-data.tar
+#wsl --unregister docker-desktop-data
+#wsl --import docker-desktop-data "D:\Program Files\Docker\wsl\data" "D:\docker-data.tar" --version 2
